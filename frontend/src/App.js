05c9484791647dcc898aa37b5ddfc1bef5fdc928
elainethepain <80547818+elainethepain@users.noreply.github.com>
2023-05-17 12:01:31 -0700

    nav css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,17 @@ import "./App.css";
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Center className="container"></Center>
+      <Center className="container">
+        <Flex className="nav_container">
+          <h1 class="word_logo"><span data-text-fill="true" class="word_logo">
+            AI EMOJI
+          </span>
+          </h1>
+          <Spacer />
+          <Button className="fuel_network" size='md'>Connect to Fuel Network</Button>
+        </Flex>
+
+      </Center>
     </ChakraProvider>
   );
 }
