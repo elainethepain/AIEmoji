507f3712898a3a2c9b8f775d7792d6c024837c66
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-01-31 23:39:03 -0800

    cha-burger

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -110,6 +110,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
