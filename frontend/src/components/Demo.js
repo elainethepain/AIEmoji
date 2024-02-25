9e7b1f1111b73fb378a1f75acc287ce1d0526efa
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-02-24 18:22:02 -0800

    well

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -124,6 +124,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}\
             {loading && info}
           </ModalBody>
 
