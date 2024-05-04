9851aae90c12e847e6592764aeabfcb0cdfc271e
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-05-03 18:00:35 -0700

    only left

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -152,6 +152,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
