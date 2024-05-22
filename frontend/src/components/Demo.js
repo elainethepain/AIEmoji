b182ebd27b2058ea84a0495b02aa258d720e2a7e
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-05-21 18:54:27 -0700

    ...ok, checked

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -159,6 +159,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
