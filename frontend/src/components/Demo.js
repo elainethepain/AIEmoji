29928278ea9143a0b36ee745838afbcff3d5909a
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-05-31 06:48:17 -0700

    will i take these next steps, or stumble fall from the pressure of it all

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -165,6 +165,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
