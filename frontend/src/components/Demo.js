16eb364b888e0fdfe6663ec726bf8e81b5c0796c
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-06-06 01:58:41 -0700

    found the way out, but aint using it

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -167,6 +167,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
