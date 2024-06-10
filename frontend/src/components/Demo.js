0c866917c33e07f2be7335665e2d6ee826c16e3c
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-06-10 07:29:21 -0700

    the power,the drive, the right time will come

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -168,6 +168,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
