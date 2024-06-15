79d8fed0c69899620523ddfb3643c6cce10f745b
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-06-15 08:04:47 -0700

    discipline

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -169,6 +169,7 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
