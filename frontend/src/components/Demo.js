5e245bef9323c18a4eb2ca646829e1e9449deea5
elainethepain <80547818+elainethepain@users.noreply.github.com>
2024-06-19 10:18:23 -0700

    trying to decipher

--- frontend/src/components/Demo.js
+++ frontend/src/components/Demo.js
@@ -172,6 +172,8 @@ function Demo() {
             {loading && info}
             {loading && info}
             {loading && info}
+            {loading && info}
+            {loading && info}
           </ModalBody>
 
           <ModalFooter>
